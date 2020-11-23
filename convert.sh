839415b5f43380aeaad4fd02336915239a6ff1a0
Viktor Eisenstadt <vsa85@gmx.de>
2020-11-23 19:46:42 +0100

    add zoned option for conversion script

--- convert.sh
+++ convert.sh
@@ -4,6 +4,10 @@ for f in $(ls -1 "$2")
 do
   if [[ "$f" == *"graphml" ]]; then
     echo "Processing $f "
-    python3 agraphml2"$1".py -f "$2""$f"
+    if [[ "$1" == "zoned" ]]; then
+      python3 agraphml2connmap.py -f "$2""$f" --zoned
+    else
+      python3 agraphml2"$1".py -f "$2""$f"
+    fi
   fi
 done
